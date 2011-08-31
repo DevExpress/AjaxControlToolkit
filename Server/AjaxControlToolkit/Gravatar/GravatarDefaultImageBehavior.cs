06f974a0b1c4012c6e7e3707603da9e41851f798
Stephen Walther <Stephen.Walther@superexpert.com>
2011-08-31 08:02:27 -0700

    Adding new Gravatar control with sample page

---
+++ Server/AjaxControlToolkit/Gravatar/GravatarDefaultImageBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AjaxControlToolkit
+{
+    public enum GravatarDefaultImageBehavior
+    {
+        Default,
+        MysteryMan,
+        Identicon,
+        MonsterId,
+        Wavatar,
+        Retro
+    }
+}
