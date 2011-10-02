178418f222ba9be45f30f0589018184182e696ce
superexpert <none@none>
2011-10-02 09:12:45 -0700

    Superexpert: Updated Readme and Assembly version

--- Server/AjaxControlToolkit/Properties/AssemblyInfo.cs
+++ Server/AjaxControlToolkit/Properties/AssemblyInfo.cs
@@ -36,11 +36,11 @@ using AjaxControlToolkit;
 #if NET4
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
 [assembly: System.Web.Script.AjaxFrameworkAssembly]
-[assembly: AssemblyVersion("4.1.50831")]
-[assembly: AssemblyFileVersion("4.1.50831")]
+[assembly: AssemblyVersion("4.1.50927")]
+[assembly: AssemblyFileVersion("4.1.50927")]
 #else
-[assembly: AssemblyVersion("3.5.50831")]
-[assembly: AssemblyFileVersion("3.5.50831")]  
+[assembly: AssemblyVersion("3.5.50927")]
+[assembly: AssemblyFileVersion("3.5.50927")]  
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 
