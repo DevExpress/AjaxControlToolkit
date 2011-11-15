3af5134fea729bb44c75c4f01abad4183c806cfe
Stephen@StephenLaptop <Stephen@StephenLaptop>
2011-11-15 11:09:12 -0800

    Superexpert: issue 26701-Fixed issue OnActiveTabIndex not firing at tab index 0

--- Server/AjaxControlToolkit/Tabs/TabContainer.cs
+++ Server/AjaxControlToolkit/Tabs/TabContainer.cs
@@ -100,7 +100,6 @@ namespace AjaxControlToolkit {
                 for (int i = 0; i <= ActiveTabIndex && i < Tabs.Count; i++) {
                     if (!Tabs[i].Visible) counter--;
                 }
-                if (counter < 0) counter = 0;
                 return counter;
             }
         }
@@ -429,7 +428,7 @@ namespace AjaxControlToolkit {
         protected override bool LoadPostData(string postDataKey, NameValueCollection postCollection) {
             int tabIndex = ActiveTabIndex;
             bool result = base.LoadPostData(postDataKey, postCollection);
-            if (tabIndex != ActiveTabIndex) {
+            if (ActiveTabIndex == 0 || tabIndex != ActiveTabIndex) {
                 return true;
             }
             return result;
@@ -483,7 +482,6 @@ namespace AjaxControlToolkit {
                 int parseIndex = eventArgument.IndexOf(":", StringComparison.Ordinal);
                 Debug.Assert(parseIndex != -1, "Expected new active tab index!");
                 if (parseIndex != -1 && Int32.TryParse(eventArgument.Substring(parseIndex + 1), out parseIndex)) {
-                    parseIndex = getServerActiveTabIndex(parseIndex);
                     if (parseIndex != ActiveTabIndex) {
                         ActiveTabIndex = parseIndex;
                         OnActiveTabChanged(EventArgs.Empty);
