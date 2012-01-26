12968fdc76bfacb54c477f57a715573195dfb81e
mohinder <Mohinder.J@superexpert.com>
2012-01-26 15:30:52 +0530

    issue27031 - Updated code after merging with master branch.

---
+++ Tests/AjaxControlToolkit.Tests/Bugs/HtmlEditorExtender/27031/WebForm1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AjaxControlToolkit.Tests.Bugs.HtmlEditorExtender._27031
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
