df3483ca56f6228ba2743cbeaf0680aa221f50de
StephenWalther <Stephen.Walther@superexpert.com>
2012-06-23 22:34:32 -0700

    Updated package files

--- Package35.bat
+++ Package35.bat
@@ -1,11 +1,22 @@
 REM Creates download package for .NET 3.5 version of Ajax Control Toolkit
 REM Requires 7za.exe (http://www.7-zip.org/download.html) at c:\zip\7za.exe
 
-
+SET MSBuildFolder=C:\Windows\Microsoft.NET\Framework\v3.5\
 SET PackageFolder=PackageForNET35
 SET BinaryFolder=%PackageFolder%\AjaxControlToolkit.Binary.NET35
 SET SanitizerProvidersFolder=%BinaryFolder%\SanitizerProviders
 SET zipper=%c:\zip\7za.exe
+
+
+REM Remove PackageFolder
+rd %PackageFolder% /s /q
+
+REM Prep for 3.5
+call Prep35.bat
+
+REM Build the Solution
+%MSBuildFolder%msbuild AjaxControlToolkit.VS2008.sln  /p:Configuration=Release /t:Clean;Build
+
 
 REM Create the package folder
 md %PackageFolder%
