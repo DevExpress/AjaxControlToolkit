e6770289ff0f4d6a1e44a99b36b0ac3d6ec4008d
Mohinder.j <Mohinder.j@superexpert.ccom>
2013-01-30 16:43:25 +0530

    AreaChartControl - added new control AreaChart and files related to this control. Added sample page with sample and description of control and its properties. Added new entry in Samples.sitemap for left link for Areachart control.

---
+++ Server/AjaxControlToolkit/AreaChart/AreaChartDesigner.cs
@@ -0,0 +1,54 @@
+﻿using System.Web.UI.WebControls;
+using System.Web.UI;
+using System.Web.UI.Design;
+using AjaxControlToolkit;
+using AjaxControlToolkit.Design;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.Design;
+using System.Windows.Forms.Design;
+using System.Windows.Forms;
+using System.Text;
+using System.Globalization;
+using System.ComponentModel.Design.Serialization;
+using System.IO;
+
+namespace AjaxControlToolkit
+{
+    /// <summary>
+    /// Control Designer for the AreaChart
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2117:AptcaTypesShouldOnlyExtendAptcaBaseTypes", Justification = "Security handled by base class")]
+    public class AreaChartDesigner : System.Web.UI.Design.ControlDesigner
+    {
+        /// <summary>
+        /// Gets reference of AreaChart component
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2116:AptcaMethodsShouldOnlyCallAptcaMethods", Justification = "Security handled by base class")]
+        private AjaxControlToolkit.AreaChart AreaChart
+        {
+            get
+            {
+                return (AjaxControlToolkit.AreaChart)Component;
+            }
+        }
+
+        /// <summary>
+        /// This returns design time html contents of the control.
+        /// </summary>
+        /// <param name="regions"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2116:AptcaMethodsShouldOnlyCallAptcaMethods", Justification = "Security handled by base class")]
+        public override string GetDesignTimeHtml(DesignerRegionCollection regions)
+        {
+            StringBuilder sb = new StringBuilder(1024);
+            sb.Append(string.Format("<div style=\"width: {0}px; height: {1}px;border-style: solid; border-width: 1px;\">", AreaChart.ChartWidth, AreaChart.ChartHeight));
+            StringWriter sr = new StringWriter(sb, CultureInfo.InvariantCulture);
+            HtmlTextWriter writer = new HtmlTextWriter(sr);
+            AreaChart.CreateChilds();
+            AreaChart.RenderControl(writer);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Server/AjaxControlToolkit/AreaChart/AreaChartSeriesCollectionEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace AjaxControlToolkit
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AreaChartSeriesCollectionEditor : CollectionEditor
+    {
+        public AreaChartSeriesCollectionEditor(Type type)
+            : base(type)
+        {
+        }
+
+        protected override Type[] CreateNewItemTypes()
+        {
+            return new Type[] { typeof(AreaChartSeries) };
+        }
+
+        protected override bool CanSelectMultipleInstances()
+        {
+            return false;
+        }
+    }
+}
