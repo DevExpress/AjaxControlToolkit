ca419cd5f5ef80179328bc6aae78f81a5603c210
Mohinder.j <Mohinder.j@superexpert.ccom>
2013-01-30 16:55:50 +0530

    BubbleChartControl - added new control and files related to this control. Also added sample page with description of control and its properties. Add new entry in Samples.sitemap for left link for BubbleChart control.

---
+++ Server/AjaxControlToolkit/BubbleChart/BubbleChart.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Configuration;
+using System.Reflection;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Web.Script;
+using System.ComponentModel;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using AjaxControlToolkit;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+
+#region [ Resources ]
+
+[assembly: System.Web.UI.WebResource("BubbleChart.BubbleChart.js", "application/x-javascript")]
+[assembly: System.Web.UI.WebResource("BubbleChart.BubbleChart.debug.js", "application/x-javascript")]
+[assembly: WebResource("BubbleChart.BubbleChart.css", "text/css", PerformSubstitution = true)]
+
+#endregion
+
+namespace AjaxControlToolkit
+{
+
+    /// <summary>
+    /// BubbleChart creates BubbleChart with the specified values.
+    /// </summary>
+    [Designer("AjaxControlToolkit.BubbleChartDesigner, AjaxControlToolkit")]
+    [RequiredScript(typeof(CommonToolkitScripts))]
+    [ClientCssResource("BubbleChart.BubbleChart.css")]
+    [ClientScriptResource("Sys.Extended.UI.BubbleChart", "BubbleChart.BubbleChart.js")]
+    public class BubbleChart : ScriptControlBase
+    {
+        private BubbleChartValueCollection bubbleChartValueList = null;
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Initializes a new BubbleChart control.
+        /// </summary>
+        public BubbleChart()
+            : base(true, HtmlTextWriterTag.Div)
+        {
+        }
+
+        #endregion
+
+        #region [Private Properties]
+        /// <summary>
+        /// Gets whether control is in design mode or not.
+        /// </summary>
+        private bool IsDesignMode
+        {
+            get
+            {
+                return (HttpContext.Current == null);
+            }
+        }
+
+        #endregion
+
+        #region [ Public Properties ]
+
+        /// <summary>
+        /// Width of Bubble Chart.
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue(null)]
+        [ClientPropertyName("chartWidth")]
+        public string ChartWidth
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Height of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue(null)]
+        [ClientPropertyName("chartHeight")]
+        public string ChartHeight
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Title of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("chartTitle")]
+        public string ChartTitle
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Provide list of values to client side. Need help from Values property 
+        /// for designer experience support, cause Editor always blocks the property
+        /// ability to provide values to client side as ExtenderControlProperty on run time.
+        /// </summary>
+        [PersistenceMode(PersistenceMode.InnerProperty)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [Browsable(false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [ExtenderControlProperty(true, true)]
+        public BubbleChartValueCollection BubbleChartClientValues
+        {
+            get
+            {
+                return bubbleChartValueList;
+            }
+        }
+
+        /// <summary>
+        /// defines values related to Bubble chart.
+        /// </summary>
+        [PersistenceMode(PersistenceMode.InnerProperty)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [DefaultValue(null)]
+        [NotifyParentProperty(true)]
+        [Editor(typeof(BubbleChartValueCollectionEditor), typeof(UITypeEditor))]
+        public BubbleChartValueCollection BubbleChartValues
+        {
+            get
+            {
+                if (bubbleChartValueList == null)
+                    bubbleChartValueList = new BubbleChartValueCollection();
+                return bubbleChartValueList;
+            }
+        }
+
+        /// <summary>
+        /// CSS/Theme file name of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue("BubbleChart")]
+        [ClientPropertyName("theme")]
+        public string Theme
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Number of lines on the Y Axis. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue(6)]
+        [ClientPropertyName("yAxisLines")]
+        public int YAxisLines
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Number of lines on the X Axis. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue(6)]
+        [ClientPropertyName("xAxisLines")]
+        public int XAxisLines
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Number of sizes of bubbles. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue(5)]
+        [ClientPropertyName("bubbleSizes")]
+        public int BubbleSizes
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Font color of title of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("chartTitleColor")]
+        public string ChartTitleColor
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Color of background lines of Value Axis of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("yAxisLineColor")]
+        public string YAxisLineColor
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Color of background lines of X Axis of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("xAxisLineColor")]
+        public string XAxisLineColor
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Color of Base Lines of Bubble Chart. 
+        /// </summary>
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("baseLineColor")]
+        public string BaseLineColor
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region [ Members ]
+
+        /// <summary>
+        /// On Init to validate data.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            if (!IsDesignMode)
+            {
+                foreach (BubbleChartValue bubbleChartValue in BubbleChartValues)
+                {
+                    if (bubbleChartValue.Category == null || bubbleChartValue.Category.Trim() == "")
+                    {
+                        throw new Exception("Category is missing the BubbleChartValue. Please provide a Category in the BubbleChartValue.");
+                    }
+
+                    if (bubbleChartValue.X == null)
+                    {
+                        throw new Exception("X is missing the BubbleChartValue. Please provide a value of X in the BubbleChartValue.");
+                    }
+
+                    if (bubbleChartValue.Y == null)
+                    {
+                        throw new Exception("Y is missing the BubbleChartValue. Please provide a value of Y in the BubbleChartValue.");
+                    }
+
+                    if (bubbleChartValue.Data == null)
+                    {
+                        throw new Exception("Data is missing the BubbleChartValue. Please provide a value of Data in the BubbleChartValue.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// CreateChilds call to create child controls for Bubble Chart.
+        /// </summary>
+        internal void CreateChilds()
+        {
+            this.Controls.Clear();
+            this.CreateChildControls();
+        }
+
+        /// <summary>
+        /// CreateChildControls creates html controls for a Bubble Chart control.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            GenerateHtmlInputControls();
+        }
+
+
+        /// <summary>
+        /// GenerateHtmlInputControls creates parent div for Bubble Chart control.
+        /// </summary>
+        /// <returns>Return the client id of parent div that contains all other html controls.</returns>
+        protected string GenerateHtmlInputControls()
+        {
+            HtmlGenericControl parent = new HtmlGenericControl("div");
+            parent.ID = "_ParentDiv";
+            parent.Attributes.Add("style", string.Format("border-style:solid; border-width:1px;width:{0};height:{1};", ChartWidth, ChartHeight));
+            Controls.Add(parent);
+
+            return parent.ClientID;
+        }
+
+        /// <summary>
+        /// DescribeComponent creates propreties in ScriptComponentDescriptor for child controls in Bubble Chart
+        /// </summary>
+        /// <param name="descriptor">Descriptor object which will accpet server components to convert in client script.</param>
+        protected override void DescribeComponent(ScriptComponentDescriptor descriptor)
+        {
+            base.DescribeComponent(descriptor);
+            if (!IsDesignMode)
+            {
+
+            }
+        }
+
+        #endregion
+
+    }
+}
