4494f9cd893586934617cecbd5c59df64b35ec6e
budiadiono <budiadiono@gmail.com>
2013-09-06 19:43:31 +0700

    Added custom controls sample & walkthrough for bundling custom control

---
+++ SampleWebSites/CustomAjaxServerControlSample/AjaxControlToolkitWelcomeLabel/ActWelcomeLabel.js
@@ -0,0 +1,26 @@
+﻿/// <reference name="MicrosoftAjax.js"/>
+
+Type.registerNamespace("CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel");
+
+CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel = function (element) {
+    CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel.initializeBase(this, [element]);
+};
+
+CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel.prototype = {
+    initialize: function() {
+        CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel.callBaseMethod(this, 'initialize');
+        var el = this.get_element();
+        $common.setStyle(el, { cursor: 'pointer' });
+        el.onclick = function () {
+            alert("You've been clicked!");
+        };
+    },
+
+    dispose: function() {
+        CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel.callBaseMethod(this, 'dispose');
+    }
+};
+
+CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel.registerClass('CustomAjaxServerControlSample.AjaxControlToolkitWelcomeLabel.ActWelcomeLabel', Sys.UI.Control);
+
+if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
