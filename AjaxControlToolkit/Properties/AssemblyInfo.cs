e51b6a201f5e0cdde196f289eb923626ad9a6843
Ilya Kh.d <ilya.kharchenko@devexpress.com>
2014-10-02 18:20:25 +0400

    blank solution created

---
+++ AjaxControlToolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyTitle("Ajax Control Toolkit")]
+[assembly: AssemblyProduct("AjaxControlToolkit")]
+[assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2014")]
+
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: ComVisible(false)]
+
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("15.1")]
+[assembly: AssemblyFileVersion("15.1")]
