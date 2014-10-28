0825112ac23d6c7b30cc55bcac2af7046aeb1817
Ilya Kh.d <ilya.kharchenko@devexpress.com>
2014-10-28 17:42:36 +0300

    NumericUpDown sample fix

---
+++ AjaxControlToolkit.SampleSite/App_Code/NumericUpDown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.Web.Script.Services.ScriptService]
+public class NumericUpDown : System.Web.Services.WebService {
+
+    [WebMethod]
+    public int NextValue(int current, string tag) {
+        return new Random().Next(Math.Min(1000, Math.Max(0, current)), 1001);
+    }
+
+    [WebMethod]
+    public int PrevValue(int current, string tag) {
+        return new Random().Next(0, Math.Min(1000, Math.Max(0, current)));
+    }
+
+}
---
+++ AjaxControlToolkit.SampleSite/NumericUpDown/NumericUpDown.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/NumericUpDown.cs" Class="NumericUpDown" %>
