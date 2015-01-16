ab39c0ace5db225f7c7715b11b176fdd41ae7793
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2015-01-16 13:49:05 +0300

    copyright year fixed

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 // WARNING this file is shared accross multiple projects
 
 [assembly: AssemblyProduct("AjaxControlToolkit")]
-[assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2014")]
+[assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2015")]
 
 [assembly: AssemblyVersion("15.1")]
 [assembly: AssemblyFileVersion("15.1")]
