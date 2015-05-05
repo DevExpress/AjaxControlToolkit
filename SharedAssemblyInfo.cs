08273cd6f32dbd585d38b96f26fc056c381f33ff
Ilya Kh.d <ilya.kharchenko@devexpress.com>
2015-05-05 18:32:21 +0300

    version updated to 15.1.2

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 [assembly: AssemblyProduct("ASP.NET AJAX Control Toolkit")]
 [assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2015")]
 
-[assembly: AssemblyVersion("15.1.1.0")]
-[assembly: AssemblyFileVersion("15.1.1.0")]
+[assembly: AssemblyVersion("15.1.2.0")]
+[assembly: AssemblyFileVersion("15.1.2.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
