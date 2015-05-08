e586ac7fe47edea32f63c0dc2f4a6f822e9a669b
Ilya Kh.d <ilya.kharchenko@devexpress.com>
2015-05-08 17:25:03 +0300

    Jasmine: static resources test made correct

--- AjaxControlToolkit.Jasmine/Suites/ToolkitResourceManager/TestExtenderStaticControl.cs
+++ AjaxControlToolkit.Jasmine/Suites/ToolkitResourceManager/TestExtenderStaticControl.cs
@@ -10,13 +10,6 @@ namespace AjaxControlToolkit.Jasmine.Suites.ToolkitResourceManager {
     [ClientScriptResource("Sys.Extended.UI.TestExtenderBehavior", "TestExtender.static")]
     [TargetControlType(typeof(TextBox))]
     public class TestExtenderStaticControl : ExtenderControlBase {
-
-        public TestExtenderStaticControl() {
-            ScriptManager.ScriptResourceMapping.AddDefinition("TestExtender.static.js", GetType().Assembly, new ScriptResourceDefinition() {
-                Path = "~/Suites/ToolkitResourceManager/Scripts/TestExtender.static.js",
-                DebugPath = "~/Suites/ToolkitResourceManager/Scripts/TestExtender.static.debug.js"
-            });
-        }
     }
 
 }
