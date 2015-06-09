6b7823dd43ad09cfde976ab6930622a2007edb45
Fadil Mamedov <fadil.mamedov@devexpress.com>
2015-06-09 15:13:10 +0300

    extension methods added

---
+++ AjaxControlToolkit.Jasmine/TestUtils/extensionMethods.js
@@ -0,0 +1,11 @@
+﻿(function() {
+
+    String.prototype.toIdSelector = function() {
+        return "#" + this.toString();
+    };
+
+    String.prototype.toClassSelector = function() {
+        return "." + this.toString();
+    };
+
+}());
