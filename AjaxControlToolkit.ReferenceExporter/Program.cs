fd19af000de811385d2cec18bba69042679d5593
Ilya Kh.d <ilya.kharchenko@devexpress.com>
2015-06-30 14:03:41 +0300

    DocGen: export to file implemented

--- AjaxControlToolkit.ReferenceExporter/Program.cs
+++ AjaxControlToolkit.ReferenceExporter/Program.cs
@@ -1,7 +1,10 @@
 ﻿using AjaxControlToolkit.Reference.Core;
+using AjaxControlToolkit.Reference.Core.Parsing;
 using AjaxControlToolkit.Reference.Core.Razor;
 using System;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace AjaxControlToolkit.ReferenceExporter {
 
@@ -17,10 +20,48 @@ namespace AjaxControlToolkit.ReferenceExporter {
             var engine = new Engine(rootDir);
             var template = engine.CreateTemplateInstance<TypeDoc>("~/Views/Reference/Type.cshtml");
 
-            var result = template.Render(new TypeDoc("AjaxControlTookit.Test"));
+            var outputDir = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Docs");
 
-            Console.WriteLine(result);
+            if(Directory.Exists(outputDir))
+                Directory.Delete(outputDir, true);
+
+            Directory.CreateDirectory(outputDir);
+
+            foreach(var doc in GetDoc().Types) {
+                Console.WriteLine(doc.Name);
+                File.WriteAllText(Path.Combine(outputDir, doc.Name + ".html"), template.Render(doc));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Done!");
             Console.ReadKey();
+        }
+
+
+        static Documentation GetDoc() {
+            var doc = new Documentation();
+            var xml = LoadXml(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AjaxControltoolkit.xml"));
+
+            var members = xml.Root.Element("members").Elements().Select(el => new RawDoc(el.Attribute("name").Value) {
+                Elements = el.Elements()
+            }).OrderBy(el => el.TargetFullName);
+
+            doc.Add(members);
+            return doc;
+        }
+
+        static XDocument LoadXml(string fileName) {
+            XDocument xml;
+            if(!System.IO.File.Exists(fileName))
+                throw new ArgumentException(String.Format("File '{0}' not found", fileName), "fileName");
+
+            xml = XDocument.Load(fileName);
+            if(xml == null)
+                throw new ArgumentException(String.Format("Unable to load XML from '{0}'", fileName), "fileName");
+
+            return xml;
         }
     }
 
