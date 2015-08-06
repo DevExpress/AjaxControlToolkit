196f9bdf01c43d219cb47e4007e273e243ed94d2
Ilya Kh.d <ilya.kharchenko@devexpress.com>
2015-08-06 12:04:03 +0300

    IDocRenderer introduced

---
+++ AjaxControlToolkit.Reference/Core/Rendering/IDocRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxControlToolkit.Reference.Core.Rendering {
+
+    public interface IDocRenderer {
+        string RenderText(string text, bool isBold, bool isItalic);
+        string RenderListItem(string text, bool isNumbered);
+        string RenderAnchor(string text, string url);
+        string RenderHeader(string text, int level);
+    }
+
+}
