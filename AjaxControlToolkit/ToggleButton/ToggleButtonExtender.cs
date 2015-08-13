05c0ed88f48f4d43aac3f4ba4a52419fbc5f4a97
Fadil Mamedov <fadil.mamedov@devexpress.com>
2015-08-13 19:13:47 +0300

    ToggleButtonExtender documentation added

--- AjaxControlToolkit/ToggleButton/ToggleButtonExtender.cs
+++ AjaxControlToolkit/ToggleButton/ToggleButtonExtender.cs
@@ -24,95 +24,143 @@ namespace AjaxControlToolkit {
         const string stringCheckedImageOverAlternateText = "CheckedImageOverAlternateText";
         const string stringUncheckedImageOverAlternateText = "UncheckedImageOverAlternateText";
 
+        /// <summary>
+        /// The width of the image.
+        /// </summary>
         [ExtenderControlProperty()]
         [RequiredProperty()]
         [DefaultValue(0)]
+        [ClientPropertyName("imageWidth")]
         public int ImageWidth {
             get { return GetPropertyValue(stringImageWidth, 0); }
             set { SetPropertyValue(stringImageWidth, value); }
         }
 
+        /// <summary>
+        /// The height of the image.
+        /// </summary>
         [ExtenderControlProperty()]
         [RequiredProperty()]
         [DefaultValue(0)]
+        [ClientPropertyName("imageHeight")]
         public int ImageHeight {
             get { return GetPropertyValue(stringImageHeight, 0); }
             set { SetPropertyValue(stringImageHeight, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is in the unchecked state.
+        /// </summary>
         [ExtenderControlProperty()]
         [RequiredProperty()]
         [DefaultValue("")]
         [UrlProperty()]
+        [ClientPropertyName("uncheckedImageUrl")]
         public string UncheckedImageUrl {
             get { return GetPropertyValue(stringUncheckedImageUrl, String.Empty); }
             set { SetPropertyValue(stringUncheckedImageUrl, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is in the checked state.
+        /// </summary>
         [ExtenderControlProperty()]
         [RequiredProperty()]
         [DefaultValue("")]
         [UrlProperty()]
+        [ClientPropertyName("checkedImageUrl")]
         public string CheckedImageUrl {
             get { return GetPropertyValue(stringCheckedImageUrl, String.Empty); }
             set { SetPropertyValue(stringCheckedImageUrl, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is disabled and in the unchecked state.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
         [UrlProperty]
+        [ClientPropertyName("disabledUncheckedImageUrl")]
         public string DisabledUncheckedImageUrl {
             get { return GetPropertyValue(stringDisabledUncheckedImageUrl, String.Empty); }
             set { SetPropertyValue(stringDisabledUncheckedImageUrl, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is disabled and in the checked state.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
         [UrlProperty]
+        [ClientPropertyName("disabledCheckedImageUrl")]
         public string DisabledCheckedImageUrl {
             get { return GetPropertyValue(stringDisabledCheckedImageUrl, String.Empty); }
             set { SetPropertyValue(stringDisabledCheckedImageUrl, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is in the checked state and the mouse is over the button.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
         [UrlProperty]
+        [ClientPropertyName("checkedImageOverUrl")]
         public string CheckedImageOverUrl {
             get { return GetPropertyValue(stringCheckedImageOverUrl, String.Empty); }
             set { SetPropertyValue(stringCheckedImageOverUrl, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is in the unchecked state and the mouse is over the button.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
         [UrlProperty]
+        [ClientPropertyName("uncheckedImageOverUrl")]
         public string UncheckedImageOverUrl {
             get { return GetPropertyValue(stringUncheckedImageOverUrl, String.Empty); }
             set { SetPropertyValue(stringUncheckedImageOverUrl, value); }
         }
 
+        /// <summary>
+        /// The alt text to show when the toggle button is in the unchecked state.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
+        [ClientPropertyName("uncheckedImageAlternateText")]
         public string UncheckedImageAlternateText {
             get { return GetPropertyValue(stringUncheckedImageAlternateText, String.Empty); }
             set { SetPropertyValue(stringUncheckedImageAlternateText, value); }
         }
 
+        /// <summary>
+        /// The alt text to show when the toggle button is in the checked state.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
+        [ClientPropertyName("checkedImageAlternateText")]
         public string CheckedImageAlternateText {
             get { return GetPropertyValue(stringCheckedImageAlternateText, String.Empty); }
             set { SetPropertyValue(stringCheckedImageAlternateText, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is in the checked state and the mouse is over the button.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
+        [ClientPropertyName("checkedImageOverAlternateText")]
         public string CheckedImageOverAlternateText {
             get { return GetPropertyValue(stringCheckedImageOverAlternateText, String.Empty); }
             set { SetPropertyValue(stringCheckedImageOverAlternateText, value); }
         }
 
+        /// <summary>
+        /// The URL of the image to show when the toggle button is in the unchecked state and the mouse is over the button.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue("")]
+        [ClientPropertyName("uncheckedImageOverAlternateText")]
         public string UncheckedImageOverAlternateText {
             get { return GetPropertyValue(stringUncheckedImageOverAlternateText, String.Empty); }
             set { SetPropertyValue(stringUncheckedImageOverAlternateText, value); }
