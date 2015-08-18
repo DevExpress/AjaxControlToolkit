18582731823577014f83edf021a925e3b8d96ba1
Fadil Mamedov <fadil.mamedov@devexpress.com>
2015-08-18 12:57:17 +0300

    RoundedCornersExtender documentation added

--- AjaxControlToolkit/RoundedCorners/RoundedCornersExtender.cs
+++ AjaxControlToolkit/RoundedCorners/RoundedCornersExtender.cs
@@ -12,29 +12,46 @@ namespace AjaxControlToolkit {
     [TargetControlType(typeof(WebControl))]
     [ToolboxBitmap(typeof(ToolboxIcons.Accessor), Constants.RoundedCornersName + Constants.IconPostfix)]
     public class RoundedCornersExtender : ExtenderControlBase {
+        /// <summary>
+        /// Gets or sets an integer value that specifies the radius of the corners (and the height of the added area). The default is 5.
+        /// </summary>
         [DefaultValue(5)]
         [ExtenderControlProperty()]
+        [ClientPropertyName("radius")]
         public int Radius {
             get { return GetPropertyValue("Radius", 5); }
             set { SetPropertyValue("Radius", value); }
         }
 
+        /// <summary>
+        /// Gets or sets a Sys.Extended.UI.BoxCorners object that specifies which corners should be rounded.
+        /// </summary>
         [DefaultValue(BoxCorners.All)]
         [ExtenderControlProperty]
+        [ClientPropertyName("corners")]
         public BoxCorners Corners {
             get { return GetPropertyValue("Corners", BoxCorners.All); }
             set { SetPropertyValue("Corners", value); }
         }
 
+        /// <summary>
+        /// Gets or sets a string that contains the background color of the rounded corner areas.
+        /// By default, this property gets the background color of the panel that it is attached to.
+        /// </summary>
         [DefaultValue(typeof(Color), "")]
         [ExtenderControlProperty]
+        [ClientPropertyName("color")]
         public Color Color {
             get { return GetPropertyValue("Color", Color.Empty); }
             set { SetPropertyValue("Color", value); }
         }
 
+        /// <summary>
+        /// Gets or sets a string that contains the color of the border and therefore of the rounded corners.
+        /// </summary>
         [DefaultValue(typeof(Color), "")]
         [ExtenderControlProperty]
+        [ClientPropertyName("borderColor")]
         public Color BorderColor {
             get { return GetPropertyValue("BorderColor", Color.Empty); }
             set { SetPropertyValue("BorderColor", value); }
