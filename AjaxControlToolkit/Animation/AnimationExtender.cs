497033fb4b3b6aed88317f8a8daf81548bd09db5
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2015-08-19 12:01:18 +0300

    Animation documentation updated

--- AjaxControlToolkit/Animation/AnimationExtender.cs
+++ AjaxControlToolkit/Animation/AnimationExtender.cs
@@ -7,7 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace AjaxControlToolkit {
 
-    // Extender used to play animations when a control's events fire
+    /// <summary>
+    /// Extender used to play animations when a control's events fire
+    /// </summary>
     [Designer(typeof(AnimationExtenderDesigner))]
     [RequiredScript(typeof(AnimationScripts))]
     [ClientScriptResource("Sys.Extended.UI.Animation.AnimationBehavior", Constants.AnimationName)]
@@ -21,55 +23,79 @@ namespace AjaxControlToolkit {
         Animation _onHoverOver;
         Animation _onHoverOut;
 
+        /// <summary>
+        /// OnLoad animation.
+        /// </summary>
         [DefaultValue(null)]
         [Browsable(false)]
         [ExtenderControlProperty]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [ClientPropertyName("onLoad")]
         public new Animation OnLoad {
             get { return GetAnimation(ref _onLoad, "OnLoad"); }
             set { SetAnimation(ref _onLoad, "OnLoad", value); }
         }
 
+        /// <summary>
+        /// OnClick animation.
+        /// </summary>
         [DefaultValue(null)]
         [Browsable(false)]
         [ExtenderControlProperty]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [ClientPropertyName("onClick")]
         public Animation OnClick {
             get { return GetAnimation(ref _onClick, "OnClick"); }
             set { SetAnimation(ref _onClick, "OnClick", value); }
         }
 
+        /// <summary>
+        /// OnMouseOver animation.
+        /// </summary>
         [DefaultValue(null)]
         [Browsable(false)]
         [ExtenderControlProperty]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [ClientPropertyName("onMouseOver")]
         public Animation OnMouseOver {
             get { return GetAnimation(ref _onMouseOver, "OnMouseOver"); }
             set { SetAnimation(ref _onMouseOver, "OnMouseOver", value); }
         }
 
+        /// <summary>
+        /// OnMouseOut animation.
+        /// </summary>
         [DefaultValue(null)]
         [Browsable(false)]
         [ExtenderControlProperty]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [ClientPropertyName("onMouseOut")]
         public Animation OnMouseOut {
             get { return GetAnimation(ref _onMouseOut, "OnMouseOut"); }
             set { SetAnimation(ref _onMouseOut, "OnMouseOut", value); }
         }
 
+        /// <summary>
+        /// OnHoverOver animation.
+        /// </summary>
         [DefaultValue(null)]
         [Browsable(false)]
         [ExtenderControlProperty]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [ClientPropertyName("onHoverOver")]
         public Animation OnHoverOver {
             get { return GetAnimation(ref _onHoverOver, "OnHoverOver"); }
             set { SetAnimation(ref _onHoverOver, "OnHoverOver", value); }
         }
 
+        /// <summary>
+        /// OnHoverOut animation.
+        /// </summary>
         [DefaultValue(null)]
         [Browsable(false)]
         [ExtenderControlProperty]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [ClientPropertyName("onHoverOut")]
         public Animation OnHoverOut {
             get { return GetAnimation(ref _onHoverOut, "OnHoverOut"); }
             set { SetAnimation(ref _onHoverOut, "OnHoverOut", value); }
