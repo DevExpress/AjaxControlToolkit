3abee814d93fc258d8aa9c46ed4012e5bc293518
Fadil Mamedov <fadil.mamedov@devexpress.com>
2015-08-21 20:58:52 +0300

    PagingBulletedListExtender documentation added

--- AjaxControlToolkit/PagingBulletedList/PagingBulletedListExtender.cs
+++ AjaxControlToolkit/PagingBulletedList/PagingBulletedListExtender.cs
@@ -16,47 +16,74 @@ namespace AjaxControlToolkit {
             EnableClientState = true;
         }
 
+        /// <summary>
+        /// Number of characters in the index headings (ignored if MaxItemPerPage is set).
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue(1)]
+        [ClientPropertyName("indexSize")]
         public int IndexSize {
             get { return GetPropertyValue<int>("IndexSize", 1); }
             set { SetPropertyValue<int>("IndexSize", value); }
         }
 
+        /// <summary>
+        /// Height of the bulleted list.
+        /// </summary>
         [ExtenderControlProperty()]
+        [ClientPropertyName("height")]
         public int? Height {
             get { return GetPropertyValue<int?>("Height", null); }
             set { SetPropertyValue<int?>("Height", value); }
         }
 
+        /// <summary>
+        /// Separator text to be placed between indices.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue(" - ")]
+        [ClientPropertyName("separator")]
         public string Separator {
             get { return GetPropertyValue<string>("Separator", " - "); }
             set { SetPropertyValue<string>("Separator", value); }
         }
 
-        //When use MaxItemPerPage IndexSize is ignored, index are automaticaly generated
+        /// <summary>
+        /// Maximum number of items per page (ignores the IndexSize property).
+        /// </summary>
         [ExtenderControlProperty()]
+        [ClientPropertyName("maxItemPerPage")]
         public int? MaxItemPerPage {
             get { return GetPropertyValue<int?>("MaxItemPerPage", null); }
             set { SetPropertyValue<int?>("MaxItemPerPage", value); }
         }
 
+        /// <summary>
+        /// Whether or not the items should be sorted client-side.
+        /// </summary>
         [ExtenderControlProperty()]
         [DefaultValue(false)]
+        [ClientPropertyName("clientSort")]
         public bool ClientSort {
             get { return GetPropertyValue<bool>("ClientSort", false); }
             set { SetPropertyValue<bool>("ClientSort", value); }
         }
 
+        /// <summary>
+        /// CSS class for the selected index.
+        /// </summary>
         [ExtenderControlProperty()]
+        [ClientPropertyName("selectIndexCssClass")]
         public string SelectIndexCssClass {
             get { return GetPropertyValue<string>("SelectIndexCssClass", String.Empty); }
             set { SetPropertyValue<string>("SelectIndexCssClass", value); }
         }
 
+        /// <summary>
+        /// CSS class for indices that aren't selected.
+        /// </summary>
         [ExtenderControlProperty()]
+        [ClientPropertyName("unselectIndexCssClass")]
         public string UnselectIndexCssClass {
             get { return GetPropertyValue<string>("UnselectIndexCssClass", String.Empty); }
             set { SetPropertyValue<string>("UnselectIndexCssClass", value); }
