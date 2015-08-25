42857c7db8b1a6354364bdac5f4dbc29763d9551
Fadil Mamedov <fadil.mamedov@devexpress.com>
2015-08-25 19:38:59 +0300

    MaskedEditExtender documentation added

--- AjaxControlToolkit/MaskedEdit/MaskedEditExtender.cs
+++ AjaxControlToolkit/MaskedEdit/MaskedEditExtender.cs
@@ -8,6 +8,11 @@ using System.Web.UI.WebControls;
 
 namespace AjaxControlToolkit {
 
+    /// <summary>
+    /// MaskedEdit is an ASP.NET AJAX extender that attaches to a TextBox control to restrict the kind
+    /// of text that can be entered. MaskedEdit applies a "mask" to the input that permits only certain
+    /// types of characters/text to be entered. The supported data formats are: Number, Date, Time, and DateTime.
+    /// </summary>
     [Designer(typeof(MaskedEditExtenderDesigner))]
     [ClientScriptResource("Sys.Extended.UI.MaskedEditBehavior", Constants.MaskedEditValidatorName)]
     [ClientScriptResource("Sys.Extended.UI.MaskedEditBehavior", Constants.MaskedEditName)]
@@ -71,6 +76,9 @@ namespace AjaxControlToolkit {
             ClientState = (string.Compare(Page.Form.DefaultFocus, TargetControlID, StringComparison.OrdinalIgnoreCase) == 0) ? "Focused" : null;
         }
 
+        /// <summary>
+        /// Mask to be applied to target TextBox.
+        /// </summary>
         [RequiredProperty()]
         [DefaultValue("")]
         [ExtenderControlProperty]
@@ -84,16 +92,31 @@ namespace AjaxControlToolkit {
             }
         }
 
+        /// <summary>
+        /// Prompt text to use when a clipboard paste is performed.
+        /// </summary>
         [DefaultValue("Your browser security settings don't permit the automatic execution of paste operations. Please use the keyboard shortcut Ctrl+V instead.")]
         [ExtenderControlProperty]
+        [ClientPropertyName("clipboardText")]
         public string ClipboardText {
             get { return GetPropertyValue<string>("ClipboardText", "Your browser security settings don't permit the automatic execution of paste operations. Please use the keyboard shortcut Ctrl+V instead."); }
             set { SetPropertyValue<string>("ClipboardText", value); }
         }
 
+        /// <summary>
+        /// Type of validation to perform.
+        /// </summary>
+        /// <remarks>
+        /// None - No validation
+        /// Number - Number validation
+        /// Date - Date validation
+        /// Time - Time validation
+        /// DateTime - Date and time validation
+        /// </remarks>
         [DefaultValue(MaskedEditType.None)]
         [ExtenderControlProperty]
         [RefreshProperties(RefreshProperties.All)]
+        [ClientPropertyName("maskType")]
         public MaskedEditType MaskType {
             get { return GetPropertyValue<MaskedEditType>("MaskType", MaskedEditType.None); }
             set {
@@ -133,114 +156,188 @@ namespace AjaxControlToolkit {
             }
         }
 
+        /// <summary>
+        /// Message displayed when editing in TextBox.
+        /// </summary>
         [DefaultValue(true)]
         [ExtenderControlProperty]
+        [ClientPropertyName("messageValidatorTip")]
         public bool MessageValidatorTip {
             get { return GetPropertyValue<bool>("MessageValidatorTip", true); }
             set { SetPropertyValue<bool>("MessageValidatorTip", value); }
         }
 
+        /// <summary>
+        /// True to show a tooltip message when the mouse hovers over an invalid TextBox.
+        /// </summary>
         [DefaultValue(false)]
         [ExtenderControlProperty]
+        [ClientPropertyName("errorTooltipEnabled")]
         public bool ErrorTooltipEnabled {
             get { return GetPropertyValue<bool>("ErrorTooltipEnabled", false); }
             set { SetPropertyValue<bool>("ErrorTooltipEnabled", value); }
         }
 
+        /// <summary>
+        /// CSS class for the tooltip message.
+        /// </summary>
         [DefaultValue("")]
         [ExtenderControlProperty]
+        [ClientPropertyName("errorTooltipCssClass")]
         public string ErrorTooltipCssClass {
             get { return GetPropertyValue<string>("ErrorTooltipCssClass", ""); }
             set { SetPropertyValue<string>("ErrorTooltipCssClass", value); }
         }
 
+        /// <summary>
+        /// True to allow copy/paste with the clipboard.
+        /// </summary>
         [DefaultValue(true)]
         [ExtenderControlProperty]
+        [ClientPropertyName("clipboardEnabled")]
         public bool ClipboardEnabled {
             get { return GetPropertyValue<bool>("ClipboardEnabled", true); }
             set { SetPropertyValue<bool>("ClipboardEnabled", value); }
         }
 
+        /// <summary>
+        /// True to automatically fill in empty mask characters not specified by the user.
+        /// </summary>
+        /// <remarks>
+        /// MaskType=Number - Empty mask characters will be filled with zeros
+        /// MaskType=Time - Empty mask characters will be filled with the current time
+        /// MaskType=Date - Empty mask characters will be filled with the current date
+        /// MaskType=DateTime - Empty mask characters will be filled with the current date/time
+        /// </remarks>
         [DefaultValue(true)]
         [ExtenderControlProperty]
+        [ClientPropertyName("autocomplete")]
         public bool AutoComplete {
             get { return GetPropertyValue<bool>("AutoComplete", true); }
             set { SetPropertyValue<bool>("AutoComplete", value); }
         }
 
+        /// <summary>
+        /// True to clear the TextBox when invalid text is entered.
+        /// </summary>
         [DefaultValue(false)]
         [ExtenderControlProperty]
+        [ClientPropertyName("clearTextOnInvalid")]
         public bool ClearTextOnInvalid {
             get { return GetPropertyValue<bool>("ClearTextOnInvalid", false); }
             set { SetPropertyValue<bool>("ClearTextOnInvalid", value); }
         }
 
+        /// <summary>
+        /// Default character to use when AutoComplete is enabled.
+        /// </summary>
         [DefaultValue("")]
         [ExtenderControlProperty]
+        [ClientPropertyName("autoCompleteValue")]
         public string AutoCompleteValue {
             get { return GetPropertyValue<string>("AutoCompleteValue", ""); }
             set { SetPropertyValue<string>("AutoCompleteValue", value); }
         }
 
+        /// <summary>
+        /// Valid characters for mask type "C" (case-sensitive).
+        /// </summary>
         [DefaultValue("")]
         [ExtenderControlProperty]
+        [ClientPropertyName("filtered")]
         public string Filtered {
             get { return GetPropertyValue<string>("Filtered", ""); }
             set { SetPropertyValue<string>("Filtered", value); }
         }
 
+        /// <summary>
+        /// Text input direction.
+        /// </summary>
+        /// <remarks>
+        /// LeftToRight - Left to Right
+        /// RightToLeft - Right to left
+        /// </remarks>
         [DefaultValue(MaskedEditInputDirection.LeftToRight)]
         [ExtenderControlProperty]
+        [ClientPropertyName("inputDirection")]
         public MaskedEditInputDirection InputDirection {
             get { return GetPropertyValue<MaskedEditInputDirection>("InputDirection", MaskedEditInputDirection.LeftToRight); }
             set { SetPropertyValue<MaskedEditInputDirection>("InputDirection", value); }
         }
 
+        /// <summary>
+        /// Prompt character.
+        /// </summary>
         [DefaultValue("_")]
         [ExtenderControlProperty]
+        [ClientPropertyName("promptCharacter")]
         public string PromptCharacter {
-            get { return GetPropertyValue<string>("PromptChar", "_"); }
-            set { SetPropertyValue<string>("PromptChar", value); }
+            get { return GetPropertyValue<string>("PromptCharacter", "_"); }
+            set { SetPropertyValue<string>("PromptCharacter", value); }
         }
 
+        /// <summary>
+        /// CSS class used when the TextBox receives focus.
+        /// </summary>
         [DefaultValue("MaskedEditFocus")]
         [ExtenderControlProperty]
+        [ClientPropertyName("onFocusCssClass")]
         public string OnFocusCssClass {
             get { return GetPropertyValue<string>("OnFocusCssClass", "MaskedEditFocus"); }
             set { SetPropertyValue<string>("OnFocusCssClass", value); }
         }
 
+        /// <summary>
+        /// CSS class used when the text is not valid.
+        /// </summary>
         [DefaultValue("MaskedEditError")]
         [ExtenderControlProperty]
+        [ClientPropertyName("onInvalidCssClass")]
         public string OnInvalidCssClass {
             get { return GetPropertyValue<string>("OnInvalidCssClass", "MaskedEditError"); }
             set { SetPropertyValue<string>("OnInvalidCssClass", value); }
         }
 
+        /// <summary>
+        /// Custom date format.
+        /// </summary>
         [DefaultValue(MaskedEditUserDateFormat.None)]
         [ExtenderControlProperty]
+        [ClientPropertyName("userDateFormat")]
         public MaskedEditUserDateFormat UserDateFormat {
             get { return GetPropertyValue<MaskedEditUserDateFormat>("UserDateFormat", MaskedEditUserDateFormat.None); }
             set { SetPropertyValue<MaskedEditUserDateFormat>("UserDateFormat", value); }
         }
 
+        /// <summary>
+        /// Custom time format.
+        /// </summary>
         [DefaultValue(MaskedEditUserTimeFormat.None)]
         [ExtenderControlProperty]
+        [ClientPropertyName("userTimeFormat")]
         public MaskedEditUserTimeFormat UserTimeFormat {
             get { return GetPropertyValue<MaskedEditUserTimeFormat>("UserTimeFormat", MaskedEditUserTimeFormat.None); }
             set { SetPropertyValue<MaskedEditUserTimeFormat>("UserTimeFormat", value); }
         }
 
+        /// <summary>
+        /// True to remove the mask when the TextBox loses focus.
+        /// </summary>
         [DefaultValue(true)]
         [ExtenderControlProperty]
+        [ClientPropertyName("clearMaskOnLostFocus")]
         public bool ClearMaskOnLostFocus {
             get { return GetPropertyValue<bool>("ClearMaskOnLostfocus", true); }
             set { SetPropertyValue<bool>("ClearMaskOnLostfocus", value); }
         }
 
+        /// <summary>
+        /// Name of culture to use (overrides the default page culture).
+        /// </summary>
         [DefaultValue("")]
         [ExtenderControlProperty]
         [RefreshProperties(RefreshProperties.All)]
+        [ClientPropertyName("cultureName")]
         public string CultureName {
             get { return GetPropertyValue<string>("CultureName", ""); }
             set {
@@ -277,98 +374,164 @@ namespace AjaxControlToolkit {
             set { SetPropertyValue<bool>("OverridePageCulture", value); }
         }
 
+        /// <summary>
+        /// Culture override.
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureDatePlaceholder")]
         public string CultureDatePlaceholder {
             get { return GetPropertyValue<string>("CultureDatePlaceholder", ""); }
             set { SetPropertyValue<string>("CultureDatePlaceholder", value); }
         }
 
+        /// <summary>
+        /// Culture override.
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureTimePlaceholder")]
         public string CultureTimePlaceholder {
             get { return GetPropertyValue<string>("CultureTimePlaceholder", ""); }
             set { SetPropertyValue<string>("CultureTimePlaceholder", value); }
         }
 
+        /// <summary>
+        /// Culture override.
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureDecimalPlaceholder")]
         public string CultureDecimalPlaceholder {
             get { return GetPropertyValue<string>("CultureDecimalPlaceholder", ""); }
             set { SetPropertyValue<string>("CultureDecimalPlaceholder", value); }
         }
 
+        /// <summary>
+        /// Culture override.
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureThousandsPlaceholder")]
         public string CultureThousandsPlaceholder {
             get { return GetPropertyValue<string>("CultureThousandsPlaceholder", ""); }
             set { SetPropertyValue<string>("CultureThousandsPlaceholder", value); }
         }
 
+        /// <summary>
+        /// Culture override.
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureDateFormat")]
         public string CultureDateFormat {
             get { return GetPropertyValue<string>("CultureDateFormat", ""); }
             set { SetPropertyValue<string>("CultureDateFormat", value); }
         }
 
+        /// <summary>
+        /// Culture override
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureCurrencySymbolPlaceholder")]
         public string CultureCurrencySymbolPlaceholder {
             get { return GetPropertyValue<string>("CultureCurrencySymbolPlaceholder", ""); }
             set { SetPropertyValue<string>("CultureCurrencySymbolPlaceholder", value); }
         }
 
+        /// <summary>
+        /// Culture override.
+        /// </summary>
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [ExtenderControlProperty]
+        [ClientPropertyName("cultureAMPMPlaceholder")]
         public string CultureAMPMPlaceholder {
             get { return GetPropertyValue<string>("CultureAMPMPlaceholder", ""); }
             set { SetPropertyValue<string>("CultureAMPMPlaceholder", value); }
         }
 
+        /// <summary>
+        /// Whether or not AM/PM is accepted on times.
+        /// </summary>
+        /// <remarks>
+        /// The default value is false.
+        /// </remarks>
         [DefaultValue(false)]
         [ExtenderControlProperty]
+        [ClientPropertyName("acceptAMPM")]
         public bool AcceptAMPM {
             get { return GetPropertyValue<bool>("AcceptAmPm", false); }
             set { SetPropertyValue<bool>("AcceptAmPm", value); }
         }
 
+        /// <summary>
+        /// True if the negative sign (-) is allowed.
+        /// </summary>
+        /// <remarks>
+        /// None - Do not show the negative sign
+        /// Left - Show the negative sign on the left of the mask
+        /// Right - Show the negative sign on the right of the mask
+        /// </remarks>
         [DefaultValue(MaskedEditShowSymbol.None)]
         [ExtenderControlProperty]
+        [ClientPropertyName("acceptNegative")]
         public MaskedEditShowSymbol AcceptNegative {
             get { return GetPropertyValue<MaskedEditShowSymbol>("AcceptNegative", MaskedEditShowSymbol.None); }
             set { SetPropertyValue<MaskedEditShowSymbol>("AcceptNegative", value); }
         }
+
+        /// <summary>
+        /// CSS class used when the TextBox gets focus with a negative value.
+        /// </summary>
         [DefaultValue("MaskedEditFocusNegative")]
         [ExtenderControlProperty]
+        [ClientPropertyName("onFocusCssNegative")]
         public string OnFocusCssNegative {
             get { return GetPropertyValue<string>("OnFocusCssNegative", "MaskedEditFocusNegative"); }
             set { SetPropertyValue<string>("OnFocusCssNegative", value); }
         }
 
+        /// <summary>
+        /// CSS class used when the TextBox loses focus with a negative value.
+        /// </summary>
         [DefaultValue("MaskedEditBlurNegative")]
         [ExtenderControlProperty]
+        [ClientPropertyName("onBlurCssNegative")]
         public string OnBlurCssNegative {
             get { return GetPropertyValue<string>("OnBlurCssNegative", "MaskedEditBlurNegative"); }
             set { SetPropertyValue<string>("OnBlurCssNegative", value); }
         }
 
+        /// <summary>
+        /// Specifies how the currency symbol is displayed.
+        /// </summary>
+        /// <remarks>
+        /// None - Do not show the currency symbol
+        /// Left - Show the currency symbol on the left of the mask
+        /// Right - Show the currency symbol on the right of the mask
+        /// </remarks>
         [DefaultValue(MaskedEditShowSymbol.None)]
         [ExtenderControlProperty]
+        [ClientPropertyName("displayMoney")]
         public MaskedEditShowSymbol DisplayMoney {
             get { return GetPropertyValue<MaskedEditShowSymbol>("DisplayMoney", MaskedEditShowSymbol.None); }
             set { SetPropertyValue<MaskedEditShowSymbol>("DisplayMoney", value); }
         }
 
+        /// <summary>
+        /// Default century used when a date mask only has two digits for the year.
+        /// </summary>
         [DefaultValue(1900)]
         [ExtenderControlProperty]
+        [ClientPropertyName("century")]
         public int Century {
             get {
                 var century = int.Parse(DateTime.Now.Year.ToString().Substring(0, 2)) * 100;
