6e7660d5dd87ceda3ac631bf9fe08449cf56e639
Fadil Mamedov <fadil.mamedov@devexpress.com>
2015-08-26 13:19:16 +0300

    DynamicPopulateExtender documentation added

--- AjaxControlToolkit/DynamicPopulate/DynamicPopulateExtender.cs
+++ AjaxControlToolkit/DynamicPopulate/DynamicPopulateExtender.cs
@@ -9,6 +9,11 @@ using System.Web.UI.WebControls;
 
 namespace AjaxControlToolkit {
 
+    /// <summary>
+    /// DynamicPopulate is an extender that replaces the contents of a control with the result of a Web service or page method call.
+    /// It allows you to dynamically populate the innerHTML of a control based on a web service callback.
+    /// The method call returns a string of HTML that is inserted as a child of the target element.
+    /// </summary>
     [Designer(typeof(DynamicPopulateExtenderDesigner))]
     [RequiredScript(typeof(CommonToolkitScripts))]
     [ClientScriptResource("Sys.Extended.UI.DynamicPopulateBehavior", Constants.DynamicPopulateName)]
@@ -16,36 +21,49 @@ namespace AjaxControlToolkit {
     [ToolboxBitmap(typeof(ToolboxIcons.Accessor), Constants.DynamicPopulateName + Constants.IconPostfix)]
     public class DynamicPopulateExtender : ExtenderControlBase {
 
+        /// <summary>
+        /// Whether or not we should clear the HTML contents of the target element when an update begins.
+        /// </summary>
         [DefaultValue(true)]
         [ExtenderControlProperty]
         [Category("Behavior")]
+        [ClientPropertyName("clearContentsDuringUpdate")]
         public bool ClearContentsDuringUpdate {
             get { return GetPropertyValue("ClearContentsDuringUpdate", true); }
             set { SetPropertyValue("ClearContentsDuringUpdate", value); }
         }
 
-        // An key that you can pass to the web service call, such as an ID.  This is optional.
+        /// <summary>
+        /// An key that you can pass to the web service call, such as an ID. This is optional.
+        /// </summary>
         [DefaultValue("")]
         [ExtenderControlProperty]
         [Category("Behavior")]
+        [ClientPropertyName("contextKey")]
         public string ContextKey {
             get { return GetPropertyValue("ContextKey", String.Empty); }
             set { SetPropertyValue("ContextKey", value); }
         }
 
-        // The ID of a control to trigger the population of the target. The population will be triggered by this controls "click" event.
+        /// <summary>
+        /// The ID of a control to trigger the population of the target. The population will be triggered by this controls "click" event.
+        /// </summary>
         [ExtenderControlProperty]
         [IDReferenceProperty(typeof(Control))]
-        [ClientPropertyName("PopulateTriggerID")]
+        [ClientPropertyName("populateTriggerID")]
         [Category("Behavior")]
         public string PopulateTriggerControlID {
             get { return GetPropertyValue("PopulateTriggerControlID", String.Empty); }
             set { SetPropertyValue("PopulateTriggerControlID", value); }
         }
 
+        /// <summary>
+        /// The method name of the web service of page method to call.
+        /// </summary>
         [ExtenderControlProperty]
         [DefaultValue("")]
         [Category("Behavior")]
+        [ClientPropertyName("serviceMethod")]
         public string ServiceMethod {
             get { return GetPropertyValue("ServiceMethod", String.Empty); }
             set {
@@ -56,11 +74,14 @@ namespace AjaxControlToolkit {
             }
         }
 
-        // The path of the web service to call, or if this is blank, a page method will be called instead.
+        /// <summary>
+        /// The path of the web service to call, or if this is blank, a page method will be called instead.
+        /// </summary>
         [ExtenderControlProperty]
         [UrlProperty]
         [TypeConverter(typeof(ServicePathConverter))]
         [Category("Behavior")]
+        [ClientPropertyName("servicePath")]
         public string ServicePath {
             get { return GetPropertyValue("ServicePath", String.Empty); }
             set { SetPropertyValue("ServicePath", value); }
@@ -70,19 +91,26 @@ namespace AjaxControlToolkit {
             return !String.IsNullOrEmpty(ServiceMethod);
         }
 
+        /// <summary>
+        /// A CSS style to apply while the update is in progress
+        /// </summary>
         [ExtenderControlProperty]
         [DefaultValue("")]
         [Category("Behavior")]
+        [ClientPropertyName("updatingCssClass")]
         public string UpdatingCssClass {
             get { return GetPropertyValue("UpdatingCss", String.Empty); }
             set { SetPropertyValue("UpdatingCss", value); }
         }
 
-        // A CustomScript can be used to eval a javascript function that will return a string to populate the control.
-        // This script method must return a string and will be called instead of the Service or Page method.
+        /// <summary>
+        /// A CustomScript can be used to eval a javascript function that will return a string to populate the control.
+        /// This script method must return a string and will be called instead of the Service or Page method.
+        /// </summary>
         [ExtenderControlProperty]
         [DefaultValue("")]
         [Category("Behavior")]
+        [ClientPropertyName("customScript")]
         public string CustomScript {
             get { return GetPropertyValue("CustomScript", String.Empty); }
             set {
@@ -93,6 +121,9 @@ namespace AjaxControlToolkit {
             }
         }
 
+        /// <summary>
+        /// Whether the results of the dynamic population should be cached and not fetched again after the first load.
+        /// </summary>
         [ExtenderControlProperty]
         [ClientPropertyName("cacheDynamicResults")]
         [DefaultValue(false)]
