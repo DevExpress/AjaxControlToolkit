e1c91de631a489fb80d92259a29c3746a27faf32
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2015-09-07 11:35:56 +0300

    DocGen: server members rendering fixed

--- AjaxControlToolkit.Reference/Controllers/ReferenceController.cs
+++ AjaxControlToolkit.Reference/Controllers/ReferenceController.cs
@@ -120,10 +120,10 @@ namespace AjaxControlToolkit.Reference.Controllers {
             var doc = new Documentation();
             var xml = LoadXml(Server.MapPath("~/bin/AjaxControltoolkit.xml"));
 
-            var elementNames = xml.Root.Element("members").Elements().Select(el => el.Attribute("name")).ToList();
+            var elementNames = xml.Root.Element("members").Elements().Select(el => el.Attribute("name").Value).ToList();
 
             var members = xml.Root.Element("members").Elements()
-                .Where(el => el.Attribute("name").Value == "T:AjaxControlToolkit." + type)
+                .Where(el => el.Attribute("name").Value.Contains("AjaxControlToolkit." + type))
                 .Select(el => new RawDoc(el.Attribute("name").Value) {
                     Elements = el.Elements()
                 })
