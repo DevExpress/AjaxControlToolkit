0a340c4decf3b32fb932ff2ce6dee96b758a2484
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2015-12-09 14:50:56 +0300

    BarChart background is transparent (CodePlex item 28003)

--- AjaxControlToolkit/Scripts/BarChart.js
+++ AjaxControlToolkit/Scripts/BarChart.js
@@ -349,9 +349,7 @@ Sys.Extended.UI.BarChart.prototype = {
         svgContents = svgContents + '<stop offset="100%" id="LinearGradient3"></stop></linearGradient>';
         svgContents = svgContents + '</defs>';
 
-        svgContents = svgContents + String.format('<path fill="none" stroke-opacity="1" fill-opacity="1" stroke-linejoin="round" stroke-linecap="square" d="M5 {0} {1} {0} {1} {2} 5 {2} z"/>', parseInt(this._chartHeight) * 1 / 10 + 5, parseInt(this._chartWidth) - 5, parseInt(this._chartHeight) - parseInt(this._chartHeight) * 1 / 10);
-        svgContents = svgContents + String.format('<path id="ChartBackGround" stroke="" d="M0 0 {0} 0 {0} {1} 0 {1} z"/>', this._chartWidth, this._chartHeight);
-        svgContents = svgContents + String.format('<path fill="#ffffff" stroke-opacity="1" fill-opacity="0" stroke-linejoin="round" stroke-linecap="square" stroke="" d="M5 {0} {1} {0} {1} {2} 5 {2} z" />', parseInt(this._chartHeight) * 1 / 10 + 5, parseInt(this._chartWidth) - 5, parseInt(this._chartHeight) - parseInt(this._chartHeight) * 1 / 10);
+        svgContents = svgContents + String.format('<rect id="ChartBackGround" width="{0}" height="{1}" style="opacity: 0"/>', this._chartWidth, this._chartHeight);
 
         return svgContents;
     },
