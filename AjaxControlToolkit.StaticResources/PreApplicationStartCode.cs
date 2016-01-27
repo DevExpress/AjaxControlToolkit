9e270ac04c96e7bac7f6baa002b3a68f6a56cd81
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2016-01-27 18:35:11 +0300

    CDN fallback added (CodePlex item 28026)

--- AjaxControlToolkit.StaticResources/PreApplicationStartCode.cs
+++ AjaxControlToolkit.StaticResources/PreApplicationStartCode.cs
@@ -17,12 +17,21 @@ namespace AjaxControlToolkit.StaticResources {
         }
 
         static void CreateScriptBundle(string bundleName) {
-            if(String.IsNullOrWhiteSpace(bundleName))
-                BundleTable.Bundles.Add(new ScriptBundle("~/Scripts/AjaxControlToolkit/Bundle", Constants.CdnPrefix + "Scripts/AjaxControlToolkit/Bundle.js")
-                    .Include(ToolkitResourceManager.GetScriptPaths()));
-            else
-                BundleTable.Bundles.Add(new ScriptBundle("~/Scripts/AjaxControlToolkit/" + bundleName + "Bundle")
-                    .Include(ToolkitResourceManager.GetScriptPaths(bundleName)));
+            if(String.IsNullOrWhiteSpace(bundleName)) {
+                var bundle = new ScriptBundle("~/Scripts/AjaxControlToolkit/Bundle", Constants.CdnPrefix + "Scripts/AjaxControlToolkit/Bundle.js")
+                    .Include(ToolkitResourceManager.GetScriptPaths());
+                AddCdnFallbackExpression(bundle);
+                BundleTable.Bundles.Add(bundle);
+            } else {
+                var bundle = new ScriptBundle("~/Scripts/AjaxControlToolkit/" + bundleName + "Bundle")
+                    .Include(ToolkitResourceManager.GetScriptPaths(bundleName));
+                AddCdnFallbackExpression(bundle);
+                BundleTable.Bundles.Add(bundle);
+            }
+        }
+
+        static void AddCdnFallbackExpression(Bundle bundle) {
+            bundle.CdnFallbackExpression = "Sys.Extended";
         }
 
         static void CreateStyleBundle(string bundleName) {
