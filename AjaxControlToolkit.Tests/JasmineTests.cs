270970d4361974ff054f8a6924c64d3c5bf1c137
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2016-04-22 14:42:11 +0300

    Selenium test

---
+++ AjaxControlToolkit.Tests/JasmineTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace AjaxControlToolkit.Tests {
+
+    [TestFixture]
+    public class JasmineTests {
+
+        [Test]
+        public void Chrome() {
+            var dir = Path.GetDirectoryName(typeof(JasmineTests).Assembly.Location);            
+            IWebDriver driver = new ChromeDriver(dir);
+            driver.Navigate().GoToUrl("http://localhost:49290");
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
