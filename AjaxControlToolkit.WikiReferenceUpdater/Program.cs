584ced2e441c40e34c20a6158fb8b012d795aa56
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2016-04-29 16:44:20 +0300

    implement wiki reference updater

---
+++ AjaxControlToolkit.WikiReferenceUpdater/Program.cs
@@ -0,0 +1,33 @@
+﻿using AjaxControlToolkit.Reference;
+using AjaxControlToolkit.Reference.Core;
+using AjaxControlToolkit.Reference.Core.Rendering;
+using System;
+using System.IO;
+
+namespace AjaxControlToolkit.WikiReferenceUpdater {
+    class Program {
+        static void Main(string[] args) {
+            var wikiRepoPath = args[0];
+            var xmlDocFolder = GetXmlDocFolder();
+            var scriptsFolder = GetScriptFolder();
+            var docRenderer = new GitHubDocRenderer();
+            var extenderDoc = new ExtenderDoc(docRenderer);
+
+            var typeNames = ToolkitTypes.GetTypeNames();
+            foreach(var typeName in typeNames) {
+                var doc = Documentation.Get(typeName, xmlDocFolder, scriptsFolder);
+                var markup = extenderDoc.BuildDoc(doc.Types);
+                var markdownFilePath = Path.Combine(wikiRepoPath, typeName.Replace("Extender", "") + ".md");
+                File.WriteAllText(markdownFilePath, markup);
+            }
+        }
+
+        static string GetScriptFolder() {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\AjaxControlToolkit\Scripts");
+        }
+
+        static string GetXmlDocFolder() {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\bin");
+        }
+    }
+}
