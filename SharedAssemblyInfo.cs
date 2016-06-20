c8915728efcb834b937f25ffad50428ad518567c
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2016-06-20 19:06:53 +0300

    update assembly copyright year

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 // WARNING this file is shared accross multiple projects
 
 [assembly: AssemblyProduct("ASP.NET AJAX Control Toolkit")]
-[assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2016")]
 [assembly: AssemblyCompany("CodePlex Foundation")]
 
 [assembly: AssemblyVersion("16.1.1.0")]
