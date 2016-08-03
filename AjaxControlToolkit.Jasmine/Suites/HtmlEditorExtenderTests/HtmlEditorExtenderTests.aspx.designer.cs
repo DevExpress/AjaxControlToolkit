64dc812a56eac9a26d83f5d22da0e4a9bf1cdacd
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-08-03 10:43:29 +0300

    Resolve #168 (#187)

--- AjaxControlToolkit.Jasmine/Suites/HtmlEditorExtenderTests/HtmlEditorExtenderTests.aspx.designer.cs
+++ AjaxControlToolkit.Jasmine/Suites/HtmlEditorExtenderTests/HtmlEditorExtenderTests.aspx.designer.cs
@@ -83,5 +83,23 @@ namespace AjaxControlToolkit.Jasmine.Suites {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button SubmitButton;
+        
+        /// <summary>
+        /// TargetForExtenderWithImageButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TargetForExtenderWithImageButton;
+        
+        /// <summary>
+        /// HtmlEditorExtenderWithImageButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.HtmlEditorExtender HtmlEditorExtenderWithImageButton;
     }
 }
