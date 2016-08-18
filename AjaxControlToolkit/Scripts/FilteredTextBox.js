0708fbf3591d186370a35ff8b339627c4ccb68ff
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-08-18 16:06:20 +0300

    restore old comments

--- AjaxControlToolkit/Scripts/FilteredTextBox.js
+++ AjaxControlToolkit/Scripts/FilteredTextBox.js
@@ -91,10 +91,12 @@ Sys.Extended.UI.FilteredTextBoxBehavior.prototype = {
         // scanCode = event.charCode
 
         // In Safari, charCode, which, and keyCode will all be filled with the same value
+        // as well as keyIdentifier, which has the string representation either as "end" or "U+00000008"
 
-        // 1) Check for ctrl/alt/meta -> bail if true        
-        // 2) Check keyCode < 0x20 -> bail
-        // 3) Special case Delete (63272) -> bail
+        // 1) Check for ctrl/alt/meta -> bail if true
+        // 2) Check for keyIdentifier.startsWith("U+") -> bail if false
+        // 3) Check keyCode < 0x20 -> bail
+        // 4) Special case Delete (63272) -> bail
 
         var scanCode;
 
