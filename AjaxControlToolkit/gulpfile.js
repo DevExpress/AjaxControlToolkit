b45e6f925159a678c55bb5bb152b33c1fb4ab14a
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-08-30 12:29:15 +0300

    add css minification

--- AjaxControlToolkit/gulpfile.js
+++ AjaxControlToolkit/gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var cssmin = require("gulp-cssmin");
 
-var paths = ['Scripts/**/*.js', '!Scripts/**/*.min.js'];
+var jsPaths = ['Scripts/**/*.js', '!Scripts/**/*.min.js'];
+var cssPaths = ['Styles/*.css', '!Styles/*.min.css'];
 
-gulp.task('minify', function () {
-    return gulp.src(paths)
+gulp.task('minify-js', function () {
+    return gulp.src(jsPaths)
       .pipe(uglify())
       .pipe(rename({
           extname: ".min.js"
       }))
       .pipe(gulp.dest('Scripts/.'));
+});
+
+gulp.task('minify-css', function () {
+    return gulp.src(cssPaths)
+      .pipe(cssmin())
+      .pipe(rename({
+          extname: ".min.css"
+      }))
+      .pipe(gulp.dest('Styles/.'));
 });
