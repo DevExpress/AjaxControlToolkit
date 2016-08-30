d52f357618c897425b01254f6be68fa6aaffdaf4
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-08-30 18:52:10 +0300

    watch for changed files repeatedly

--- AjaxControlToolkit/gulpfile.js
+++ AjaxControlToolkit/gulpfile.js
@@ -1,26 +1,34 @@
-/// <binding BeforeBuild='minify-js, minify-css' />
+/// <binding ProjectOpened='minify-css, minify-js' />
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 var jsPaths = ['Scripts/**/*.js', '!Scripts/**/*.min.js'];
+var jsDest = 'Scripts/.';
 var cssPaths = ['Styles/*.css', '!Styles/*.min.css'];
+var cssDest = 'Styles/.';
 
 gulp.task('minify-js', function () {
-    return gulp.src(jsPaths)
+	return gulp.src(jsPaths)
+		.pipe(plumber())
+	  .pipe(watch(jsPaths))	  
       .pipe(uglify())
       .pipe(rename({
           extname: '.min.js'
       }))
-      .pipe(gulp.dest('Scripts/.'));
+      .pipe(gulp.dest(jsDest));
 });
 
 gulp.task('minify-css', function () {
     return gulp.src(cssPaths)
+		.pipe(plumber())
+      .pipe(watch(cssPaths))
       .pipe(cssmin())
       .pipe(rename({
           extname: '.min.css'
       }))
-      .pipe(gulp.dest('Styles/.'));
+      .pipe(gulp.dest(cssDest));
 });
