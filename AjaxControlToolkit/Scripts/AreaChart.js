18152346fa1bc8920309aa92b959986d53fd0f50
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-08-31 12:14:26 +0300

    fix corrupted  AreaChart.js

--- AjaxControlToolkit/Scripts/AreaChart.js
+++ AjaxControlToolkit/Scripts/AreaChart.js
@@ -1,4 +1,4 @@
-﻿Type.regissterNamespace("Sys.Extended.UI");
+﻿Type.registerNamespace("Sys.Extended.UI");
 
 Sys.Extended.UI.AreaChart = function(element) {
     Sys.Extended.UI.AreaChart.initializeBase(this, [element]);
