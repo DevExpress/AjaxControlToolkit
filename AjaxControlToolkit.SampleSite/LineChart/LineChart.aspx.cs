dc956c65e7b9645b2f920f174f708e6b0cb2c279
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-10-18 11:39:08 +0300

    add LineChart markup highlight

--- AjaxControlToolkit.SampleSite/LineChart/LineChart.aspx.cs
+++ AjaxControlToolkit.SampleSite/LineChart/LineChart.aspx.cs
@@ -8,5 +8,6 @@ using System.Web.UI.WebControls;
 public partial class LineChart_LineChart : System.Web.UI.Page {
 
     protected void Page_Load(object sender, EventArgs e) {
+        MarkupHighlighter.HighlightControlMarkup(LineChart1.ID, codeInfoBlock);
     }
 }
