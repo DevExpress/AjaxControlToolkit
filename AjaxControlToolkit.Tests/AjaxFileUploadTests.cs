f238b227d84d4734a595c0c69fd833b46ba5fb61
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2017-05-03 13:21:31 +0300

    add test cleanup

--- AjaxControlToolkit.Tests/AjaxFileUploadTests.cs
+++ AjaxControlToolkit.Tests/AjaxFileUploadTests.cs
@@ -8,6 +8,18 @@ namespace AjaxControlToolkit.Tests {
 
     [TestFixture]
     public class AjaxFileUploadTests {
+        string _tempFolder;
+
+        [OneTimeSetUp]
+        public void Init() {
+            _tempFolder = AjaxFileUpload.BuildRootTempFolder();
+        }
+
+        [SetUp]
+        public void ClearTempFoder() {
+            if(Directory.Exists(_tempFolder))
+                Directory.Delete(_tempFolder, true);
+        }
 
         [Test]
         public void NotAllowedFileExtensionIsBlocked() {
@@ -21,6 +33,7 @@ namespace AjaxControlToolkit.Tests {
             var request = new WorkerRequest("------WebKitFormBoundaryCqenIHPHe1ZTCr0d\r\nContent-Disposition: form-data; name=\"act-file-data\"; filename=\"zero.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundaryCqenIHPHe1ZTCr0d--\r\n", "filename=aaa.jpg&fileId=testtemp", "multipart/form-data; boundary=----WebKitFormBoundaryCqenIHPHe1ZTCr0d");
             var context = new HttpContext(request);
             AjaxFileUploadHelper.Process(context);
+            Assert.True(File.Exists(Path.Combine(_tempFolder, "testtemp", "aaa.jpg")));
         }
             var context = new HttpContext(request);
             AjaxFileUploadHelper.Process(context);
