39df4991d2239dede45cb4c2967e34f64bf52886
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2017-05-03 13:04:39 +0300

    fix tests

--- AjaxControlToolkit.Tests/AjaxFileUploadTests.cs
+++ AjaxControlToolkit.Tests/AjaxFileUploadTests.cs
@@ -11,14 +11,17 @@ namespace AjaxControlToolkit.Tests {
 
         [Test]
         public void NotAllowedFileExtensionIsBlocked() {
-            var request = new SimpleWorkerRequest("", "", "", "fileName=aaa.exe", new StringWriter());
+            var request = new WorkerRequest("", "fileName=aaa.exe", "");
             var context = new HttpContext(request);
             Assert.Throws<Exception>(() => AjaxFileUploadHelper.Process(context));
         }
-
+        
         [Test]
         public void AllowedFileExtensionIsAccepted() {
-            var request = new WorkerRequest("------WebKitFormBoundaryCqenIHPHe1ZTCr0dContent - Disposition: form - data; name = \"act-file-data\"; filename = \"zero.jpg\"Content - Type: image / jpeg------WebKitFormBoundaryCqenIHPHe1ZTCr0d--\r\n", "filename=aaa.jpg", "multipart/form-data; boundary=----WebKitFormBoundaryCqenIHPHe1ZTCr0d");
+            var request = new WorkerRequest("------WebKitFormBoundaryCqenIHPHe1ZTCr0d\r\nContent-Disposition: form-data; name=\"act-file-data\"; filename=\"zero.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundaryCqenIHPHe1ZTCr0d--\r\n", "filename=aaa.jpg&fileId=testtemp", "multipart/form-data; boundary=----WebKitFormBoundaryCqenIHPHe1ZTCr0d");
+            var context = new HttpContext(request);
+            AjaxFileUploadHelper.Process(context);
+        }
             var context = new HttpContext(request);
             AjaxFileUploadHelper.Process(context);
         }
