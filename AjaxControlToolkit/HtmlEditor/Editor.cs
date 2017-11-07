2e64da693a4cb91c7279ebabbcfd7df68aee1512
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2017-11-07 16:19:02 +0300

    remove redundant using

--- AjaxControlToolkit/HtmlEditor/Editor.cs
+++ AjaxControlToolkit/HtmlEditor/Editor.cs
@@ -2,7 +2,6 @@ using AjaxControlToolkit.Design;
 using AjaxControlToolkit.HtmlEditor.Sanitizer;
 using AjaxControlToolkit.HtmlEditor.ToolbarButtons;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
