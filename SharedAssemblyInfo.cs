bce957530df0ff52e515eed321171c1746ad973c
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2018-01-16 13:59:05 +0300

    upadate version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2018")]
 [assembly: AssemblyCompany("CodePlex Foundation")]
 
-[assembly: AssemblyVersion("18.1.0.0")]
-[assembly: AssemblyFileVersion("18.1.0.0")]
+[assembly: AssemblyVersion("18.1.1.0")]
+[assembly: AssemblyFileVersion("18.1.1.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
