fe4fa4892edb8231d526f1ab3050b48276338526
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2020-05-20 13:08:16 +0300

    Update version, year

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Resources;
 // WARNING this file is shared accross multiple projects
 
 [assembly: AssemblyProduct("ASP.NET AJAX Control Toolkit")]
-[assembly: AssemblyCopyright("Copyright (c) 2012-2019, CodePlex Foundation")]
+[assembly: AssemblyCopyright("Copyright (c) 2012-2020, CodePlex Foundation")]
 [assembly: AssemblyCompany("CodePlex Foundation")]
 
-[assembly: AssemblyVersion("19.1.0.0")]
-[assembly: AssemblyFileVersion("19.1.0.0")]
+[assembly: AssemblyVersion("20.1.0.0")]
+[assembly: AssemblyFileVersion("20.1.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
